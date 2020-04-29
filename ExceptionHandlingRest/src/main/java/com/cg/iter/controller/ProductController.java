b58070caa85ee170d17635b8ac48cf705db50135
PlabanRoy <61134603+PlabanRoy@users.noreply.github.com>
2020-04-30 00:29:15 +0530

    Update ProductController.java

--- ExceptionHandlingRest/src/main/java/com/cg/iter/controller/ProductController.java
+++ ExceptionHandlingRest/src/main/java/com/cg/iter/controller/ProductController.java
@@ -16,9 +16,9 @@ public class ProductController {
     
     @GetMapping("/")
     public String createSample() {
-        Product p1 = new Product(101,"Windows 10 PRO","64Bit OS for Desktop & Laptops",8000D);
-        Product p2 = new Product(102,"Mac Desktop","64Bit OS for Desktop & Laptops",0D);
-        Product p3 = new Product(103,"Linux Mint 19","64Bit OS for Desktop & Laptops",0D);
+        Product p1 = new Product(501,"Windows 10 PRO","64Bit OS for Desktop & Laptops",8000D);
+        Product p2 = new Product(502,"Mac Desktop","64Bit OS for Desktop & Laptops",0D);
+        Product p3 = new Product(503,"Linux Mint 19","64Bit OS for Desktop & Laptops",0D);
         service.create(p1);
         service.create(p2);
         service.create(p3);
